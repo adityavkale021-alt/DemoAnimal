920a32693a8841570196dc81448d38a488dd97ab
sawan <sawan@Aditya_kale>
2026-06-22 12:56:43 +0530

    avc

--- DemoAnimal/src/main/java/com/example/demo/controller/AnimalController.java
+++ DemoAnimal/src/main/java/com/example/demo/controller/AnimalController.java
@@ -5,12 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Animal;
 import com.example.demo.service.AnimalService;
 
 
-
+@RestController
 public class AnimalController {
 	
 	@Autowired
